85cda9c04d25cd8977eebf4bc605d27f53624f42
Chris Lambe <chris@chrislambe.com>
2013-08-26 11:40:05 -0500

    Turns out that this.paper.getById(stateShape.id) is exactly the same as stateShape.

--- jquery.usmap.js
+++ jquery.usmap.js
@@ -611,7 +611,7 @@
             {
               stateShape = this.stateShapes[abbreviation];
               for(style in this.options[property])
-                this.paper.getById(stateShape.id).attr(style,this.options[property][style]);
+                stateShape.attr(style,this.options[property][style]);
             }
             break;
           case "stateSpecificStyles":
@@ -619,7 +619,7 @@
             {
               stateShape = this.stateShapes[abbreviation];
               for(style in this.options[property][abbreviation])
-                this.paper.getById(stateShape.id).attr(style,this.options[property][abbreviation][style]);
+                stateShape.attr(style,this.options[property][abbreviation][style]);
             }
             break;
         }
